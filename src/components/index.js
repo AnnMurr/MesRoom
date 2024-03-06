6c2fad295a200caf43532c7204520d8c3d08cd02
AnnMurr <113177152+AnnMurr@users.noreply.github.com>
2024-03-06 15:51:32 +0200

    fix

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
-export { Body } from "./components/common/body/body";
-export { LinkGenerator } from "./components/pages/linkGenerator/linkGenerator";
-export { Main } from "./components/pages/main/main";
-export { Room } from "./components/pages/room/room";
-export { Chat } from "./components/pages/chat/chat";
+export { Body } from "./common/body/body";
+export { LinkGenerator } from "./pages/linkGenerator/linkGenerator";
+export { Main } from "./pages/main/main";
+export { Room } from "./pages/room/room";
+export { Chat } from "./pages/chat/chat";
