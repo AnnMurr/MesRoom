4c4d95b3a703097f9fdb99b54c2055a568d08456
AnnMurr <113177152+AnnMurr@users.noreply.github.com>
2024-03-21 16:28:47 +0200

    fix

--- src/components/pages/main/main.jsx
+++ src/components/pages/main/main.jsx
@@ -73,7 +73,7 @@ export const Main = () => {
         return (
                 <>
                         <Section>
-                                {isLoading ?
+                                {!isLoading ?
                                         <LogoCube
                                                 cubeGreySide={cubeGreySide}
                                                 cubeGreenSide={cubeGreenSide}
